0d90c4c7a4cf82ee8763a4ca2bf2fed8d6432580
Blair Conrad <blair@blairconrad.com>
2017-01-05 20:09:22 -0500

    Add test to make sure serviceFactory can't be null.

---
+++ src/SelfInitializingFakes/SelfInitializingFake.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfInitializingFakes
+{
+    using System;
+
+    /// <summary>
+    /// A fake that will delegate to an actual service when first used, creating a script
+    /// that will be used on subsequent uses.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to fake.</typeparam>
+    public class SelfInitializingFake<TService>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelfInitializingFake{TService}"/> class.
+        /// </summary>
+        /// <param name="serviceFactory">A factory that will create a concrete factory if needed.</param>
+        public SelfInitializingFake(Func<TService> serviceFactory)
+        {
+            if (serviceFactory == null)
+            {
+                throw new ArgumentNullException(nameof(serviceFactory));
+            }
+        }
+    }
+}
---
+++ tests/SelfInitializingFakes.AcceptanceTests/Creation.cs
@@ -0,0 +1,27 @@
+﻿namespace SelfInitializingFakes.AcceptanceTests
+{
+    using System;
+    using FluentAssertions;
+    using Xbehave;
+    using Xunit;
+
+    public static class Creation
+    {
+        public interface IService
+        {
+        }
+
+        [Scenario]
+        public static void CreatingFromNullServiceFactory(Func<IService> serviceFactory, Exception exception)
+        {
+            "Given a null service factory"
+                .x(() => serviceFactory = null);
+
+            "When I create a self-initializing fake from the factory"
+                .x(() => exception = Record.Exception(() => new SelfInitializingFake<IService>(serviceFactory)));
+
+            "Then the constructor throws an exception"
+                .x(() => exception.Should().BeOfType<ArgumentNullException>());
+        }
+    }
+}
