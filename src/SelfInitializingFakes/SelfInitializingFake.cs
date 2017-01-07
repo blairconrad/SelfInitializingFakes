526c2664f6a6d639f1764eff2ce3b23bad321585
Blair Conrad <blair@blairconrad.com>
2017-01-07 07:28:22 -0500

    Introduce SelfInitializingFake.For

--- src/SelfInitializingFakes/SelfInitializingFake.cs
+++ src/SelfInitializingFakes/SelfInitializingFake.cs
@@ -1,30 +1,34 @@
 ﻿namespace SelfInitializingFakes
 {
     using System;
+    using FakeItEasy;
 
     /// <summary>
-    /// A fake that will delegate to an actual service when first used, creating a script
-    /// that will be used on subsequent uses.
+    /// Creates self-initializing fakes; fakes that will delegate to an actual service when first
+    /// used, creating a script that will be used on subsequent uses.
     /// </summary>
-    /// <typeparam name="TService">The type of the service to fake.</typeparam>
-    public class SelfInitializingFake<TService>
+    public static class SelfInitializingFake
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SelfInitializingFake{TService}"/> class.
+        /// Creates a new self-initializing fake <typeparamref name="TService"/>.
         /// </summary>
-        /// <param name="repository">A source of saved call information, or sink for the same.</param>
+        /// <typeparam name="TService">The type of the service to fake.</typeparam>
         /// <param name="serviceFactory">A factory that will create a concrete factory if needed.</param>
-        public SelfInitializingFake(ISavedCallRepository repository, Func<TService> serviceFactory)
+        /// <param name="repository">A source of saved call information, or sink for the same.</param>
+        /// <returns>A new self-initializing fake <typeparamref name="TService"/>.</returns>
+        public static TService For<TService>(Func<TService> serviceFactory, ISavedCallRepository repository)
         {
+            if (serviceFactory == null)
+            {
+                throw new ArgumentNullException(nameof(serviceFactory));
+            }
+
             if (repository == null)
             {
                 throw new ArgumentNullException(nameof(repository));
             }
 
-            if (serviceFactory == null)
-            {
-                throw new ArgumentNullException(nameof(serviceFactory));
-            }
+            return A.Fake<TService>();
         }
     }
 }
--- tests/SelfInitializingFakes.AcceptanceTests/Creation.cs
+++ tests/SelfInitializingFakes.AcceptanceTests/Creation.cs
@@ -18,25 +18,20 @@
             Func<IService> serviceFactory,
             Exception exception)
         {
-            "Given a saved call repository"
-                .x(() => repository = A.Fake<ISavedCallRepository>());
-
             "Given a null service factory"
                 .x(() => serviceFactory = null);
 
+            "And a saved call repository"
+                .x(() => repository = A.Fake<ISavedCallRepository>());
+
             "When I create a self-initializing fake from the factory"
-                .x(
-                    () =>
-                        exception =
-                            Record.Exception(() => new SelfInitializingFake<IService>(repository, serviceFactory)));
+                .x(() => exception = Record.Exception(() =>
+                        SelfInitializingFake.For<IService>(serviceFactory, repository)));
 
             "Then the constructor throws an exception"
-                .x(
-                    () =>
-                        exception.Should()
-                            .BeOfType<ArgumentNullException>()
-                            .Which.ParamName.Should()
-                            .Be("serviceFactory"));
+                .x(() => exception.Should()
+                    .BeOfType<ArgumentNullException>()
+                    .Which.ParamName.Should().Be("serviceFactory"));
         }
 
         [Scenario]
@@ -45,27 +40,27 @@
             Func<IService> serviceFactory,
             Exception exception)
         {
-            "Given a null saved call repository"
-                .x(() => repository = null);
-
-            "And a service factory"
+            "Given a service factory"
                 .x(() => serviceFactory = A.Fake<Func<IService>>());
 
+            "And a null saved call repository"
+                .x(() => repository = null);
+
             "When I create a self-initializing fake from the factory"
-                .x(
-                    () =>
-                        exception =
-                            Record.Exception(() => new SelfInitializingFake<IService>(repository, serviceFactory)));
+                .x(() => exception = Record.Exception(() =>
+                        SelfInitializingFake.For<IService>(serviceFactory, repository)));
 
             "Then the constructor throws an exception"
-                .x(() => exception.Should().BeOfType<ArgumentNullException>().Which.ParamName.Should().Be("repository"));
+                .x(() => exception.Should()
+                    .BeOfType<ArgumentNullException>()
+                    .Which.ParamName.Should().Be("repository"));
         }
 
         [Scenario]
         public static void CreatingFromRepositoryAndServiceFactory(
             ISavedCallRepository repository,
             Func<IService> serviceFactory,
-            SelfInitializingFake<IService> fake)
+            IService fake)
         {
             "Given a saved call repository"
                  .x(() => repository = A.Fake<ISavedCallRepository>());
@@ -74,7 +69,7 @@
                 .x(() => serviceFactory = A.Fake<Func<IService>>());
 
             "When I create a self-initializing fake from the factory"
-                .x(() => fake = new SelfInitializingFake<IService>(repository, serviceFactory));
+                .x(() => fake = SelfInitializingFake.For<IService>(serviceFactory, repository));
 
             "Then the fake is created"
                 .x(() => fake.Should().NotBeNull());
