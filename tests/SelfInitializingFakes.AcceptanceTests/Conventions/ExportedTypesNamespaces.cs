75834d3cf72bcc7eec4d8458bb433244111a4cbd
Blair Conrad <blair@blairconrad.com>
2017-01-07 20:08:12 -0500

    Add convention tests
     - ensure that a type is expored iff its namespace is
       SelfInitializingFakes

---
+++ tests/SelfInitializingFakes.AcceptanceTests/Conventions/ExportedTypesNamespaces.cs
@@ -0,0 +1,26 @@
+﻿namespace SelfInitializingFakes.AcceptanceTests.Conventions
+{
+    using System.Linq;
+    using System.Reflection;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ExportedTypesNamespaces
+    {
+        [Fact]
+        public void Exported_types_should_have_SelfInitializingFakes_namespace()
+        {
+            typeof(SelfInitializingFake).GetTypeInfo().Assembly.GetExportedTypes()
+                .Where(t => t.Namespace != "SelfInitializingFakes")
+                .Should().BeEmpty();
+        }
+
+        [Fact]
+        public void All_types_in_SelfInitializingFakes_namespace_should_be_exported()
+        {
+            typeof(SelfInitializingFake).GetTypeInfo().Assembly.GetTypes()
+                .Where(t => t.Namespace == "SelfInitializingFakes" && !t.GetTypeInfo().IsVisible)
+                .Should().BeEmpty();
+        }
+    }
+}
