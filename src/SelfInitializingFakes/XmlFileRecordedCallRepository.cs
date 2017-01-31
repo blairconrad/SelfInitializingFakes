617afba5e20c828c2b775fd45d662ced35d595b9
Blair Conrad <blair@blairconrad.com>
2017-01-30 22:19:32 -0500

    Add XmlFileRecordedCallRepository

---
+++ src/SelfInitializingFakes/XmlFileRecordedCallRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace SelfInitializingFakes
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// Saves and loads recorded calls made to a service. The calls will be saved to a file,
+    /// serialized as XML.
+    /// </summary>
+    public class XmlFileRecordedCallRepository : IRecordedCallRepository
+    {
+        private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(RecordedCall[]));
+
+        private readonly string path;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlFileRecordedCallRepository"/> class.
+        /// </summary>
+        /// <param name="path">The file to save calls to, or load them from.</param>
+        public XmlFileRecordedCallRepository(string path)
+        {
+            this.path = path;
+        }
+
+        /// <summary>
+        /// Saves recorded calls for later use.
+        /// </summary>
+        /// <param name="calls">The recorded calls to save.</param>
+        public void Save(IEnumerable<RecordedCall> calls)
+        {
+            using (var fileStream = File.Create(this.path))
+            {
+                Serializer.Serialize(fileStream, calls.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Loads and returns saved calls.
+        /// </summary>
+        /// <returns>The saved calls, or <c>null</c> if there are none.</returns>
+        public IEnumerable<RecordedCall> Load()
+        {
+            if (!File.Exists(this.path))
+            {
+                return null;
+            }
+
+            using (var fileStream = File.OpenRead(this.path))
+            {
+                return (IEnumerable<RecordedCall>)Serializer.Deserialize(fileStream);
+            }
+        }
+    }
+}
---
+++ tests/SelfInitializingFakes.Tests/Acceptance/XmlSerialization.cs
@@ -0,0 +1,89 @@
+﻿namespace SelfInitializingFakes.Tests.Acceptance
+{
+    using System;
+    using System.IO;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Xbehave;
+
+    public static class XmlSerialization
+    {
+        public interface IService
+        {
+            void VoidMethod(string s, out int i, ref DateTime dt);
+
+            Guid NonVoidMethod();
+        }
+
+        [Scenario]
+        public static void SerializeVoidCall(
+            string path,
+            IRecordedCallRepository repository,
+            IService realServiceWhileRecording,
+            int voidMethodOutInteger,
+            DateTime voidMethodRefDateTime,
+            Guid nonVoidMethodResult)
+        {
+            "Given a file path"
+                .x(() => path = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".xml"));
+
+            "And a XmlFileRecordedCallRepository targeting that path"
+                .x(() => repository = new XmlFileRecordedCallRepository(path));
+
+            "And a real service to wrap while recording"
+                .x(() =>
+                {
+                    realServiceWhileRecording = A.Fake<IService>();
+
+                    int i;
+                    DateTime dt = DateTime.MinValue;
+                    A.CallTo(() => realServiceWhileRecording.VoidMethod("firstCallKey", out i, ref dt))
+                        .AssignsOutAndRefParameters(17, new DateTime(2017, 1, 24));
+
+                    A.CallTo(() => realServiceWhileRecording.NonVoidMethod())
+                        .Returns(new Guid("6c7d8912-802a-43c0-82a2-cb811058a9bd"));
+                });
+
+            "When I use a self-initializing fake in recording mode"
+                .x(() =>
+                {
+                    using (var fakeService = SelfInitializingFake.For(() => realServiceWhileRecording, repository))
+                    {
+                        var fake = fakeService.Fake;
+                        fake.VoidMethod("firstCallKey", out voidMethodOutInteger, ref voidMethodRefDateTime);
+                        nonVoidMethodResult = fake.NonVoidMethod();
+                    }
+                });
+
+            "And I use a self-initializing fake in playback mode"
+                .x(() =>
+                {
+                    using (var playbackFakeService = SelfInitializingFake.For<IService>(() => null, repository))
+                    {
+                        var fake = playbackFakeService.Fake;
+                        int i;
+                        DateTime dt = DateTime.MinValue;
+                        fake.VoidMethod("blah", out i, ref dt);
+                    }
+                });
+
+            "Then the recording fake forwards calls to the wrapped service"
+                .x(() =>
+                {
+                    int i;
+                    DateTime dt = new DateTime(2017, 1, 24);
+                    A.CallTo(() => realServiceWhileRecording.VoidMethod(A<string>._, out i, ref dt))
+                        .MustHaveHappened();
+                    A.CallTo(() => realServiceWhileRecording.NonVoidMethod()).MustHaveHappened();
+                });
+
+            "And the playback fake returns the recorded out and ref parameters and results"
+                .x(() =>
+                {
+                    voidMethodOutInteger.Should().Be(17);
+                    voidMethodRefDateTime.Should().Be(new DateTime(2017, 1, 24));
+                    nonVoidMethodResult.Should().Be(new Guid("6c7d8912-802a-43c0-82a2-cb811058a9bd"));
+                });
+        }
+    }
+}
