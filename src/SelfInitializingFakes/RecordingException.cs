cd1232f7ee0a64504021667a5cf431b02b317a6d
Blair Conrad <blair@blairconrad.com>
2018-07-06 14:57:36 -0400

    Update to Microsoft.CodeAnalysis.FxCopAnalyzers 2.6.1

--- src/SelfInitializingFakes/RecordingException.cs
+++ src/SelfInitializingFakes/RecordingException.cs
@@ -1,12 +1,18 @@
 ﻿namespace SelfInitializingFakes
 {
     using System;
+#if FEATURE_BINARY_SERIALIZATION
+    using System.Runtime.Serialization;
+#endif
 
     /// <summary>
     /// An exception thrown when an error is encountered while
     /// using a <see cref="SelfInitializingFake{T}"/> in recording
     /// mode.
     /// </summary>
+#if FEATURE_BINARY_SERIALIZATION
+    [Serializable]
+#endif
     public class RecordingException : Exception
     {
         /// <summary>
@@ -37,5 +43,23 @@
             : base(message, innerException)
         {
         }
+
+#if FEATURE_BINARY_SERIALIZATION
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecordingException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="info"/> parameter is null.
+        /// </exception>
+        /// <exception cref="SerializationException">
+        /// The class name is null or <see cref="Exception.HResult"/> is zero (0).
+        /// </exception>
+        protected RecordingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
     }
 }
