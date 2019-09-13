5dced59de9fa3933d88b5ebc2dd95a60068331e2
Blair Conrad <blair@blairconrad.com>
2019-09-13 12:35:51 -0400

    Update tool dependencies

--- src/SelfInitializingFakes/XmlFileRecordedCallRepository.cs
+++ src/SelfInitializingFakes/XmlFileRecordedCallRepository.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Xml;
     using System.Xml.Serialization;
 
     /// <summary>
@@ -39,9 +40,12 @@
         /// <returns>The deserialized calls.</returns>
         protected override IEnumerable<RecordedCall> ReadFromStream(FileStream fileStream)
         {
+            using (var reader = XmlReader.Create(fileStream))
+            {
 #pragma warning disable CA3075 // Insecure DTD processing in XML - the framework is for testing, so presumably is run in a safe environment
-            return (IEnumerable<RecordedCall>)Serializer.Deserialize(fileStream);
+                return (IEnumerable<RecordedCall>)Serializer.Deserialize(reader);
 #pragma warning restore CA3075 // Insecure DTD processing in XML
+            }
         }
     }
 }
